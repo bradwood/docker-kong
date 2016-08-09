6847c7bdd42791a48edf645e55dd043c71112f4b
Bradley Wood <git@bradleywood.com>
2016-08-09 12:47:57 +0100

    fixed shell script

---
+++ Mountpoints/ubuntu-dev/bprc-scripts/clean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rm bprc.out*
+rm *.output
+rm bprc.log
+rm *.out
--- Mountpoints/ubuntu-dev/bprc-scripts/create_all_apis.sh
+++ Mountpoints/ubuntu-dev/bprc-scripts/create_all_apis.sh
@@ -3,13 +3,12 @@
 echo Truncated logfile...
 cp /dev/null bprc.log
 
-for f in $(ls -1 create*api.yml)
+for f in $(ls -1t create*api.yml)
 do
 	cat $f | bprc \
 	--log-level=debug \
 	--output-format=raw-all \
-	--skip-http-errors \
-	--output-file=$f.output
+	--skip-http-errors > /dev/null
 done
 
 echo Quick grep of the output files for any HTTP errors
